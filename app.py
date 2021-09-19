2ce63ae9292b20109c564fe13e0ce93c732363c7
Aishwarya Velumani <62029435+AishwaryaVelumani@users.noreply.github.com>
2021-09-19 20:31:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ from market import app
 
 if __name__=='__main__': #check if run file has executed directly
     #app.run(host='0.0.0.0',port=5000,debug=os.environ.get('DEBUG')=='1')
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0',port=80)
 #imports all the files in the right order
